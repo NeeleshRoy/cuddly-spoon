bf13f12fe5ec956191554e3517ddcec2079a950c
A1282526 <A1282526@bestbuy.com>
2021-09-29 00:19:45 -0500

    problem set 2

---
+++ 02.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    const sorted = arr.sort();
+    const min = sorted.slice(0, 3).reduce((prev, curr) => prev + curr);
+    const max = sorted.slice(-4).reduce((prev, curr) => prev + curr);
+
+    console.log(min + " " + max);
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
