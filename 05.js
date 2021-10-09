0aebf3ed891995094528b259a7d611255d6f2195
A1282526 <A1282526@bestbuy.com>
2021-10-09 08:22:41 -0500

    #5

---
+++ 05.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(a) {
+    const singleOccurence = [];
+    for (let i = 0; i < a.length; i++) {
+        if (singleOccurence.includes(a[i])) {
+            singleOccurence.splice(singleOccurence.indexOf(a[i]), 1);
+        } else {
+            singleOccurence.push(a[i]);
+        }
+    }
+
+    return singleOccurence[0]
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
